20203961212b2e6ccf48e055b8d2c741dee74df0
pradiplamsal152 <pradip.lamsal152@gmail.com>
2016-11-30 12:33:06 +0545

    Add files via upload

---
+++ prd.c
@@ -0,0 +1,24 @@
+/*
+student name: pradip lamsal
+subject: programming fundamental
+program wap a program to use assignment operation
+date:30/11/2016
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+int a,b;
+printf("enter first no");
+scanf("%d",&a);
+
+printf("enter second no");
+scanf("%d",&b);
+
+a+=b;
+printf("+=%d",a);
+getch();
+return (0);
+}
+
