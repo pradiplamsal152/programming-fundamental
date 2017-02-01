6c9edc2ce176410826f661920d94ef7c8761d878
pradiplamsal152 <pradip.lamsal152@gmail.com>
2017-02-01 11:11:49 +0545

    Add files via upload

---
+++ array.c
@@ -0,0 +1,19 @@
+/*
+student name: pradip lamsal
+subject: fundamental programming
+program:wap
+lab sheet:24
+date: 1 feb 2017
+*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a[5]={1,2,3,4,5};
+    int i;
+    for(i=0; i<=5; i++)
+    {
+        printf("a[%d]=%d\n",i,a[i]);
+    }
+    return 0;
+}
---
+++ arrayy.c
@@ -0,0 +1,20 @@
+/*
+student name: pradip lamsal
+subject: fundamental programming
+program:wap
+lab sheet:24
+date: 1 feb 2017
+*/
+#include<stdio.h>
+int main(){
+int a[10],i,sum;
+for(i=0;i<10;i++)
+    {
+printf("enter a number");
+scanf("%d",&a[i]);
+}
+for(i=0;i<10;i++){
+printf("a[%d]=%d\n", i,a[i]);
+return 0;
+}
+}
